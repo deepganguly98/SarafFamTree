1547b18f5112ff573cd06f393554854e3a7d1952
Deep Ganguly <33273794+deepganguly98@users.noreply.github.com>
2020-02-09 19:37:47 +0530

    Tiberwala family tree addition
    new family tree added with improved navigation features

---
+++ collapsable3_5.js
@@ -0,0 +1,105 @@
+
+    var chart_config = {
+        chart: {
+            container: "#collapsable-example",
+            //levelSeparation:    25,
+            //siblingSeparation:  70,
+            //subTeeSeparation:   70,
+            //nodeAlign: "BOTTOM",
+            //scrollbar: "fancy",
+            //padding: 35,
+            
+            // connectors: {
+            //     type: "curve",
+            //     style: {
+            //         "stroke-width": 2,
+            //         "stroke-linecap": "round",
+            //         "stroke": "#ccc"
+            //     }
+            // },
+            //animateOnInit: true,
+            
+            node: {
+                collapsable: true
+            },
+            animation: {
+                nodeAnimation: "easeOutBounce",
+                nodeSpeed: 700,
+                connectorsAnimation: "bounce",
+                connectorsSpeed: 700
+            }
+        },
+        nodeStructure: {
+            //collapsed:true,
+			text: {
+				title:"Sedu Mal Tibrewala"
+            },
+            //collapsed:true,
+            children:[
+                {
+                    text:{
+                        title:"Mohan Lal Tibrewala",
+                        name:"(either son or grand son of Sedu Mal)"
+                    },
+                    children:[
+                        {
+                            text:{
+                                title:"Kanhaiya Lal Tibrewala"
+                            },
+                            children:[
+                                {
+                                    text:{
+                                        title:"Bisheshwar Lal Tibrewala ",
+                                        name:"Suanti Devi"
+                                    },
+                                    collapsed:true,
+                                    children:[
+                                        {
+                                            text:{
+                                                title:"Madan Lal Tibrewala"
+                                            }
+                                        },
+                                        {
+                                            text:{
+                                                title:"Kishan Lal Tibrewala"
+                                            }
+                                        },
+                                        {
+                                            text:{
+                                                title:"Kesori Lal Tibrewala"
+                                            },
+                                            children:[
+                                                {
+                                                    text:{
+                                                        title:" Naveen Tibrewala"
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            text:{
+                                                title:"Purni Devi ",
+                                                name:"Babu Lal  Bagaria"
+                                            }
+                                        },
+                                        {
+                                            text:{
+                                                title:"Mohari Devi ",
+                                                name:"Nagar Mal Bagaria"
+                                            }
+                                        },
+                                        {
+                                            text:{
+                                                title:"Anchi Devi"
+                                            }
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+
